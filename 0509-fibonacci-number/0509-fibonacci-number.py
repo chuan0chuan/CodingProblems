39dc63631c631696c199978e32b8989423527842
chuan0chuan <chuantian00@outlook.com>
2024-11-10 22:51:35 -0600

    Time: 23 ms (98.86%), Space: 16.8 MB (6.9%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -3,7 +3,7 @@ class Solution:
         if n == 0:
             return 0
 
-        dp = [0]  * (n + 1)
+        dp = [0] * (n + 1)
 
         dp[0] = 0
         dp[1] = 1
@@ -11,5 +11,6 @@ class Solution:
         for i in range(2, n + 1):
 
             dp[i] = dp[i - 1] + dp[i - 2]
+            print(f"After iteration {i}: dp = {dp}")  # 打印当前 dp 数组
 
         return dp[n]
