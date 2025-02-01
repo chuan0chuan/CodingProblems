0cb287b3f000a891353bd7c83a698a8346871c4b
chuan0chuan <chuantian00@outlook.com>
2025-01-31 19:35:08 -0600

    Time: 3 ms (64.07%), Space: 17.9 MB (23.04%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.py
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -3,18 +3,21 @@ class Solution:
         result = []
         self.backTrack(s, 0, [], result)
         return result
-    def backTrack(self, s , startIndex, path, result):
+    
+    def backTrack(self, s: str, startIndex, path, result):
         if startIndex == len(s):
             result.append('.'.join(path[:]))
-            return
-        for i in range(startIndex, startIndex + 3):
+            return 
+
+        for i in range(startIndex, startIndex +3):
             if s[startIndex] == '0' and i > startIndex:
                 break
-            if (4 - len(path))*3 < len(s) - i or 4 - len(path) > len(s) - i:
+            if (4 - len(path)) * 3 < len(s) - i or 4 -len(path) > len(s) - i:
                 break
-            if i - startIndex == 2:
-                if not int(s[startIndex: i + 1]) <= 255:
+            if i - startIndex == 2 :
+                if int(s[startIndex : i + 1]) > 255:
                     break
-            path.append(s[startIndex: i +1])
-            self.backTrack(s, i + 1,path,result)
+            path.append(s[startIndex : i + 1])
+            self.backTrack(s, i +1 ,path,result)
             path.pop()
+    
