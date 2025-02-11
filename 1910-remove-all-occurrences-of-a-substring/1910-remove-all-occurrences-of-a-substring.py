aa90a56ee311e23a633010fd82363cab77c5db20
chuan0chuan <chuantian00@outlook.com>
2025-02-11 14:25:59 -0600

    Time: 16 ms (18.42%), Space: 17.8 MB (76.11%) - LeetHub

---
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.py
@@ -0,0 +1,15 @@
+class Solution:
+    def removeOccurrences(self, s: str, part: str) -> str:
+        n = len(part)
+        result = []
+        for c in s:
+            result.append(c)
+            if len(result) >= n and self.find_part_inend(result, part, n):
+                for i in range(n):
+                    result.pop()
+        return "".join(result)
+
+    def find_part_inend(self,result, part, length):
+        return "".join(result[-length:]) == part
+
+
