1e8cd85e85cab90085eefb6430b87b17ebda4171
chuan0chuan <chuantian00@outlook.com>
2025-02-22 20:30:03 -0600

    Time: 11 ms (53.46%), Space: 19 MB (50.66%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -8,7 +8,6 @@ class Solution:
         for key , value in freqMap.items():
             heapq.heappush(max_heap, (-value, key))
 
-
         result = ""
         # 当堆不为空时，每次弹出一个元素，(-freq, char) 中 freq 为负数
         while max_heap:
