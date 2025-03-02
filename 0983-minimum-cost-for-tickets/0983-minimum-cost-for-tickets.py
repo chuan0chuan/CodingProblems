af9726d2d0551c824a73cfa715c08d1b67b0b70c
chuan0chuan <chuantian00@outlook.com>
2025-03-01 23:09:32 -0600

    Time: 3 ms (72.75%), Space: 18 MB (48.29%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
@@ -8,7 +8,7 @@ class Solution:
                 dp[i] = dp[i-1]
             else:
                 cost1 = dp[i -1] +costs[0]
-                cost2 = max(0,dp[i-7]) + costs[1]
-                cost3 = max(0,dp[i-30]) + costs[2]
+                cost2 = dp[max(0, i - 7)] + costs[1]
+                cost3 = dp[max(0, i - 30)]  + costs[2]
                 dp[i] = min(cost1, cost2, cost3)
         return dp[365]
