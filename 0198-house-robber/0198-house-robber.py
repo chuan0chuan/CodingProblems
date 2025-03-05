47aa9f81f1bc1703431cca430dbecc2258a86141
chuan0chuan <chuantian00@outlook.com>
2025-03-04 20:04:44 -0600

    Time: 0 ms (100%), Space: 17.9 MB (12.86%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,12 +1,17 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        dp = [0] * n 
+       # step 1. define the meaning of dp
+        dp = [0] * n
+        #step2. figure out how to initialize 0
         dp[0] = nums[0]
+
         if n == 1:
             return dp[0]
-        dp[1] = max(nums[0], nums[1])
+        dp[1]=max(nums[1], dp[0])
+
         for i in range(2, n):
-            dp[i] = max(dp[i-2] + nums[i], nums[i])
+            #step3. know the way of status update based on previous record
+            dp[i] = max(dp[i-2] + nums[i], dp[i-1])
 
         return dp[-1]
