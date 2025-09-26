272892e52f4bbfaa3209838057a4b735a199e198
chuan0chuan <chuantian00@outlook.com>
2025-09-26 10:38:20 -0500

    Time: 0 ms (100%), Space: 18 MB (20.33%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,24 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        # GPT vision
+        values = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        total = 0
+        n = len(s)
+        
+        for i in range(n):
+            # 如果不是最后一个字符，并且当前值 < 下一个值，就减去当前值
+            if i < n - 1 and values[s[i]] < values[s[i + 1]]:
+                total -= values[s[i]]
+            else:
+                total += values[s[i]]
+        
+        return total
