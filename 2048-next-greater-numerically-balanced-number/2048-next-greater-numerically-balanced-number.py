96cf4591e59b8cb288e35056ccd7cde153a87b28
chuan0chuan <chuantian00@outlook.com>
2025-11-01 12:08:36 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2048-next-greater-numerically-balanced-number/2048-next-greater-numerically-balanced-number.py
+++ 2048-next-greater-numerically-balanced-number/2048-next-greater-numerically-balanced-number.py
@@ -1,3 +1,7 @@
+nums = []
+generate(0, [0]*10, nums)
+nums.sort()
+
 def generate(num: int, count: list[int], nums: list[int]) -> None:
     if num > 0 and is_beautiful(count):
         nums.append(num)
@@ -16,9 +20,6 @@ def is_beautiful(count: list[int]) -> bool:
             return False
     return True
 
-nums = []
-generate(0, [0]*10, nums)
-nums.sort()
 
 class Solution:    
     def nextBeautifulNumber(self, n: int) -> int:
