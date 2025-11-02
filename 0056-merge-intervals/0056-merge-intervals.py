b51e34ed968f6376a026092439781b2d495aa4e2
chuan0chuan <chuantian00@outlook.com>
2025-11-02 15:19:00 -0600

    Time: 11 ms (30.03%), Space: 20.9 MB (65.03%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -3,19 +3,14 @@ class Solution:
         if not intervals:
             return []
         
-        row = len(intervals)
-        
-        # 1. 先按照起始位置排序
-        intervals.sort(key=lambda x: x[0])
-        
+        intervals.sort(key = lambda x:x[0])
+
         result = []
         
-        for i in range(row):
-            # 2. 如果 result 为空 或者 当前区间不重叠，直接加入
+        for i in range(len(intervals)):
             if not result or result[-1][1] < intervals[i][0]:
                 result.append(intervals[i])
             else:
-                # 3. 如果有重叠，合并区间，更新 result 里最后一个区间的结束时间
                 result[-1][1] = max(result[-1][1], intervals[i][1])
         
         return result
