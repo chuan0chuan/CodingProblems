3bf4d3b6f00239ea1539cf42ffe63b40c0cb6b5e
chuan0chuan <chuantian00@outlook.com>
2025-11-02 16:13:13 -0600

    Time: 7 ms (96.06%), Space: 18.2 MB (96.18%) - LeetHub

--- 1169-invalid-transactions/1169-invalid-transactions.py
+++ 1169-invalid-transactions/1169-invalid-transactions.py
@@ -4,39 +4,34 @@ class Solution:
     def invalidTransactions(self, transactions: List[str]) -> List[str]:
         groups = defaultdict(list)
 
-        # 1️⃣ 解析并分组
         for t in transactions:
             name, time, amount, city = t.split(',')
             groups[name].append((int(time), int(amount), city, t))
-
+        
         res = []
 
-        # 2️⃣ 处理每个用户
-        for name, trans_list in groups.items():
-            # 按时间升序排列，方便 60 分钟剪枝
-            trans_list.sort(key=lambda x: x[0])
-            n = len(trans_list)
-            is_invalid = [False] * n  # 每笔交易一个标记位
+        for name, user_txns in groups.items():
+            user_txns.sort(key = lambda x :x[0])
+
+            n = len(user_txns)
+            is_invalid = [False] * n
 
             for i in range(n):
-                time_i, amount_i, city_i, raw_i = trans_list[i]
+                time_i, amount_i, city_i, raw_i = user_txns[i]
 
-                # 条件1：金额超限
                 if amount_i > 1000:
                     is_invalid[i] = True
+                
+                for j in range(i+1 , n):
+                    time_j, amount_j, city_j, raw_j = user_txns[j]
 
-                # 条件2：60 分钟内不同城市
-                for j in range(i + 1, n):
-                    time_j, amount_j, city_j, raw_j = trans_list[j]
                     if time_j - time_i > 60:
                         break
                     if city_i != city_j:
                         is_invalid[i] = True
                         is_invalid[j] = True
-
-            # 收集该用户所有被标记的交易
+            
             for i in range(n):
                 if is_invalid[i]:
-                    res.append(trans_list[i][3])
-
+                    res.append(user_txns[i][3])
         return res
