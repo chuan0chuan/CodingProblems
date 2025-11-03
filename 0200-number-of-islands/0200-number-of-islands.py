476c8816503c43508dfabf84a2ca7bc5be4586f5
chuan0chuan <chuantian00@outlook.com>
2025-11-02 19:52:36 -0600

    Time: 250 ms (51.31%), Space: 20.1 MB (68.2%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -5,20 +5,18 @@ class Solution:
         rows, cols = len(grid), len(grid[0])
         count = 0
 
-        def bfs(r, c):
-            q = deque([(r, c)])
+        def dfs(r ,c):
+            if r < 0 or c < 0 or r >= rows or c >= cols or grid[r][c] =='0':
+                return
             grid[r][c] = '0'
-            while q:
-                x , y = q.popleft()
-                for dx, dy in [(1,0), (-1,0), (0,1),(0,-1)]:
-                    nx, ny = x + dx, y +dy
-                    if 0 <= nx < rows and 0 <= ny < cols and grid[nx][ny] == '1':
-                        grid[nx][ny] = '0'
-                        q.append((nx,ny))
-
+            dfs(r + 1, c)
+            dfs(r-1, c)
+            dfs(r, c+1)
+            dfs(r, c -1)
+        
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c]=='1':
                     count += 1
-                    bfs(r,c)
+                    dfs(r,c)
         return count
