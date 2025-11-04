1d89b10f86855f0c896254a88dc1b1c9931375e6
chuan0chuan <chuantian00@outlook.com>
2025-11-04 12:20:53 -0600

    Time: 895 ms (32.56%), Space: 18.3 MB (57.22%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,14 +1,15 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        min_coin = [amount + 1] *(amount + 1)
-        min_coin[0] = 0
-        for current in range(1, amount + 1):
-            for coin in coins:
-                if current - coin >= 0 :
-                    min_coin[current] = min(min_coin[current],  1 + min_coin[current-coin])
-        if min_coin[-1] != (amount + 1):
-            return min_coin[-1]
-        else:
+        dp = [float('inf')] * (amount + 1)  # 创建动态规划数组，初始值为正无穷大
+        dp[0] = 0  # 初始化背包容量为0时的最小硬币数量为0
+
+        for coin in coins:  # 遍历硬币列表，相当于遍历物品
+            for i in range(coin, amount + 1):  # 遍历背包容量
+                if dp[i - coin] != float('inf'):  # 如果dp[i - coin]不是初始值，则进行状态转移
+                    dp[i] = min(dp[i - coin] + 1, dp[i])  # 更新最小硬币数量
+
+        if dp[amount] == float('inf'):  # 如果最终背包容量的最小硬币数量仍为正无穷大，表示无解
             return -1
+        return dp[amount]  # 返回背包容量为amount时的最小硬币数量
 
             
